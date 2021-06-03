e9fcfcbcda22a5b1ba5616bf39fef8f66a02aa96
Anastasia <anastas1a982@icloud.com>
2021-06-03 23:36:33 +0300

    InputSection component and redux

---
+++ src/components/InputSection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './InputSection.style.css';
+
+const InputSection = () => {
+  
+
+  return (
+    <div className="InputSection__container">
+      InputSection component
+    </div>
+  );
+};
+
+export default InputSection;
