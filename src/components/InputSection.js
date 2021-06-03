555e2209b04270f2d4bb2b51c35786d60d1e5384
Anastasia <anastas1a982@icloud.com>
2021-06-03 23:50:57 +0300

    InputSection styles added

--- src/components/InputSection.js
+++ src/components/InputSection.js
@@ -1,12 +1,79 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import noteActions from '../redux/actions/noteActions';
+import inputActions from '../redux/actions/inputActions';
 import './InputSection.style.css';
 
 const InputSection = () => {
   
+  const id = useSelector(state => state.inputs.id);
+  const title = useSelector(state => state.inputs.title);
+  const content = useSelector(state => state.inputs.content);
+  const dispatch = useDispatch();
+
+  const addNote = () => {
+    if (title && content) {
+      dispatch(noteActions.addNote({
+        title,
+        content
+      }))
+      dispatch(inputActions.resetInputs())
+    }
+  }
+
+  const updateNote = () => {
+    if (title && content) {
+      dispatch(noteActions.updateNote(id, {
+        title, content
+      }))
+      dispatch(inputActions.resetInputs())
+    }
+  }
+
+  const deleteNote = () => {
+    dispatch(noteActions.deleteNote(id))
+    dispatch(inputActions.resetInputs())
+  }
 
   return (
-    <div className="InputSection__container">
-      InputSection component
+    <div className="inputSection__container">
+       <h2>Your note</h2>
+      <input
+        className="input"
+        type="text"
+        placeholder="Note title"
+        value={title}
+        onChange={e =>
+          dispatch(inputActions.setInputTitle(e.target.value))
+        }
+      />
+      <textarea
+        className="textarea"
+        placeholder="Note content"
+        value={content}
+        onChange={e =>
+          dispatch(inputActions.setInputContent(e.target.value))
+        }
+      ></textarea>
+      <div
+        className="inputSection_container_btnWrapper"
+      >
+        <button
+          className="button"
+          onClick={id === -1 ? addNote : updateNote}
+        >
+          {id === -1 ? "ADD NOTE" : "UPDATE NOTE"}
+        </button>
+        {id !== -1 &&
+          <button
+            className="button"
+            onClick={deleteNote}
+            style={{ marginLeft: '1em', backgroundColor: 'red' }}
+          >
+            DELETE NOTE
+          </button>
+        }
+      </div>
     </div>
   );
 };
